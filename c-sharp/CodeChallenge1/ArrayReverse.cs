582bea553832a186a37b755c65c4c81e631382d3
DJ <epicmrjones@gmail.com>
2023-07-12 14:05:21 -0500

    Update ArrayReverse.cs

--- c-sharp/CodeChallenge1/ArrayReverse.cs
+++ c-sharp/CodeChallenge1/ArrayReverse.cs
@@ -5,8 +5,16 @@ namespace CodeChallenges
     {
         public static int[] ReverseArray(int[] array)
         {
+            int[] tempArray = new int[array.Length];
+            int taLength = tempArray.Length; //5
+            //loops help us traverse an array
+            for (int i = array.Length; i > 0 ; i--)
+            {
+              tempArray[taLength - i] = array[i - 1];
+              //Console.WriteLine(array[i]);
+            }
 
-            return array;
+      return tempArray;
         }
     }
 }
