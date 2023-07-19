c25318872da0a9fe0423c81e83833aad2bca12d8
DJ <epicmrjones@gmail.com>
2023-07-19 09:35:29 -0500

    fib

--- c-sharp/CodeChallenge4/FibonacciAndRowSums.cs
+++ c-sharp/CodeChallenge4/FibonacciAndRowSums.cs
@@ -1,19 +1,64 @@
-namespace CodeChallenges 
+namespace CodeChallenges
 {
-    public class CodeChallenge4
+  public class CodeChallenge4
+  {
+    public static void Main(string[] args)
     {
-        public static int GetNthNumberInFibonacciSequence(int nthNumberInSequence)
+      try
+      {
+        int n;
+        do
         {
-            int result = 0;
-
-            return result;
-        }
-
-        public static int[] RowSums(int[][] matrix)
+          Console.WriteLine("Welcome to Fibonacci's! What can we getcha? Positive Whole Numbers ONLY");
+          n = int.Parse(Console.ReadLine());
+          Console.WriteLine($"{GetNthNumberInFibonacciSequence(n)} is the nth Fib value where n = {n}");
+        }while (n > 0);
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine("You goofed! BEGONE!");
+      }
+      
+    }
+    public static int GetNthNumberInFibonacciSequence(int nthNumberInSequence)
+    {
+      int result = 0;
+      result += Fib(nthNumberInSequence);
+      //Fib(5) = Fib(4) + Fib(3)
+      //8 = 5 + 3
+      return result;
+    }
+    public static int Fib(int n)
+    {
+      if (n < 2)
+      {
+        //Console.WriteLine($"F{n} is {n}");
+        switch (n)
         {
-            int[] rowSums = new int[matrix[0].Length];
-            return rowSums;
+          case 0:
+            n = 0;
+            return n;
+          case 1:
+            n = 1;
+            return n;
         }
+      }
+      int nmo = Fib(n - 1);
+      int nmt = Fib(n - 2);
+      int result = nmo + nmt;
+
+      //Console.WriteLine($"F{n} is {result}");
+
+      return result;
+        
 
     }
+
+    public static int[] RowSums(int[][] matrix)
+    {
+      int[] rowSums = new int[matrix[0].Length];
+      return rowSums;
+    }
+
+  }
 }
